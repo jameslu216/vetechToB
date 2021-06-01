80441e418853dedca07f15bce48bc0113c41619e
YuHao <howard50209880522@gmail.com>
2021-06-01 12:42:38 +0800

    debug

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -15,7 +15,7 @@ class Doctor extends User
 
     public function user()
     {
-        return $this->belongsTo('App\Models\User', 'user_id', 'id')->first();
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
     }
 
     public function name()
