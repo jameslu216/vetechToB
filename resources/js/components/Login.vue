b9d6b3ca345970beab25b65586ce8b8b04272235
James jr <jameslu91223@gmail.com>
2021-06-10 06:03:40 +0800

    加入登入註冊WIP

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,25 +1,72 @@
 <template>
   <!-- Default form login -->
-  <form class="display_mid">
-    <p class="h4 text-center mb-4">Sign in</p>
-    <label for="defaultFormLoginEmailEx" class="grey-text">Your email</label>
-    <input type="email" id="defaultFormLoginEmailEx" class="form-control"/>
-    <br/>
-    <label for="defaultFormLoginPasswordEx" class="grey-text">Your password</label>
-    <input type="password" id="defaultFormLoginPasswordEx" class="form-control"/>
+  <div v-if="!hasLogin" class="display_mid">
+    <p class="h4 text-center mb-4">登入</p>
+    <label for="defaultFormLoginEmailEx" class="grey-text">帳號信箱</label>
+    <input
+      type="email"
+      id="defaultFormLoginEmailEx"
+      class="form-control"
+      v-model="email"
+    />
+    <br />
+    <label for="defaultFormLoginPasswordEx" class="grey-text">密碼</label>
+    <input
+      type="password"
+      id="defaultFormLoginPasswordEx"
+      class="form-control"
+      v-model="password"
+    />
     <div class="text-center mt-4">
-      <button class="btn btn-indigo" type="submit">Login</button>
+      <button
+        class="btn btn-primary"
+        style="background-color: #25d366"
+        v-on:click="login()"
+      >
+        登入
+      </button>
     </div>
-  </form>
+  </div>
+  <div v-else>
+    <span>登入成功 你可以預約了 但我沒空做</span>
+  </div>
+
   <!-- Default form login -->
 </template>
 <script>
-  import { mdbInput, mdbBtn } from 'mdbvue';
-  export default {
-    name: 'Basic',
-    components: {
-      mdbInput,
-      mdbBtn
+import httpAPI from "../httpAPI.js";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  computed:{
+    hasLogin(){
+      return this.$store.state.login.hasLogin;
+    }
+  },
+  mounted(){
+
+  },
+  methods: {
+    login() {
+      const vm=this
+      let loginRequirements = {
+        email: this.email,
+        password: this.password,
+      };
+      httpAPI.userLogin(loginRequirements).then(function (response) {
+          console.log('222',response);
+
+        try {
+          vm.$store.commit("SET_HAS_LOGIN", true);
+        } catch {}
+      });
     },
-  }
+  },
+};
 </script>
