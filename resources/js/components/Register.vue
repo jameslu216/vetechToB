c3e3fa95678eb57557b87a539eb35b20a9d5d60f
James jr <jameslu91223@gmail.com>
2021-06-09 18:01:51 +0800

    增加登入註冊頁

---
+++ resources/js/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- Default form register -->
+  <form class="display_mid">
+    <p class="h4 text-center mb-4">Sign up</p>
+    <label for="defaultFormRegisterNameEx" class="grey-text">Your name</label>
+    <input type="text" id="defaultFormRegisterNameEx" class="form-control"/>
+    <br/>
+    <label for="defaultFormRegisterEmailEx" class="grey-text">Your email</label>
+    <input type="email" id="defaultFormRegisterEmailEx" class="form-control"/>
+    <br/>
+    <label for="defaultFormRegisterConfirmEx" class="grey-text">Confirm your email</label>
+    <input type="email" id="defaultFormRegisterConfirmEx" class="form-control"/>
+    <br/>
+    <label for="defaultFormRegisterPasswordEx" class="grey-text">Your password</label>
+    <input type="password" id="defaultFormRegisterPasswordEx" class="form-control"/>
+    <div class="text-center mt-4">
+      <button class="btn btn-unique" type="submit">Register</button>
+    </div>
+  </form>
+  <!-- Default form register -->
+</template>
+<script>
+import { mdbInput, mdbBtn } from 'mdbvue';
+  export default {
+    name: 'Basic',
+    components: {
+      mdbInput,
+      mdbBtn
+    }
+  }
+</script>
+<style lang="scss">
+.format_mid{
+    display:block;
+    margin:auto;
+}
+</style>
