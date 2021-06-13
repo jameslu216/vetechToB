eb82f77887c0e4ffdf4d5e1560ded48135abbc00
jack111331 <jack111331@gmail.com>
2021-06-13 14:56:09 +0800

    fix get reservation info api

--- app/Http/Repositorys/ReservationRepository.php
+++ app/Http/Repositorys/ReservationRepository.php
@@ -180,10 +180,11 @@ class ReservationRepository
     {
         // find() only works with single-column keys, so we use where() here
         $doctors_data = [];
-        Clinic::find($clinic_id)->doctors()->each(function($doctor) use (&$doctors_data, $date) {
+        Clinic::find($clinic_id)->doctors()->get()->each(function($doctor) use (&$doctors_data, $date) {
+            
             // Append doctor data to doctors
             $doctors_data[] = [
-                'doctor_id' => $doctor->id,
+                'doctor_id' => $doctor->user_id,
                 'doctor_name' => $doctor->name(),
                 'diagnosis_time_list' => $doctor->reservations()->whereDate('datetime', '=', $date)->get()->each(function(&$diagnosis_info) {
                     $diagnosis_time = Datetime::createFromFormat('Y-m-d H:i:s', $diagnosis_info['datetime']);
@@ -191,6 +192,7 @@ class ReservationRepository
                 }
                 )->makeHidden(['id', 'clinic_id', 'phone', 'pet_name', 'pet_variety', 'pet_gender', 'pet_age', 'note', 'customer_name', 'customer_id', 'doctor_id', 'datetime']),
             ];
+            
         });
         return $doctors_data;
     }
